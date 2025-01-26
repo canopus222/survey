d45c1b268c960be9c835c86e3030212b871e04b1
star_ <star_berry2s@yahoo.co.jp>
2025-01-26 22:42:05 +0900

    Update SQLite schema with survey table definition

--- sql/sqlite.sql
+++ sql/sqlite.sql
@@ -1,4 +1,8 @@
-CREATE TABLE IF NOT EXISTS member (
-    id           INTEGER NOT NULL PRIMARY KEY,
-    name         VARCHAR(255)
+CREATE TABLE IF NOT EXISTS survey (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    age INTEGER NOT NULL,
+    job ENUM('会社員', '自営業', '学生', '主婦（夫）', 'その他') NOT NULL,
+    drinks TEXT NOT NULL,  -- よく飲むお酒（カンマ区切りで複数選択）
+    remarks TEXT  -- 備考
 );
