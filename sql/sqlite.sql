f8636ad242a68a6b10af1b31f9bcf84cd84e4dda
star_ <star_berry2s@yahoo.co.jp>
2025-01-27 17:29:52 +0900

    Fix SQLite schema

--- sql/sqlite.sql
+++ sql/sqlite.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS survey (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(255) NOT NULL,
     age INTEGER NOT NULL,
-    job ENUM('会社員', '自営業', '学生', '主婦（夫）', 'その他') NOT NULL,
+    job TEXT NOT NULL,     -- 職業（ユーザーの選択肢を保存）
     drinks TEXT NOT NULL,  -- よく飲むお酒（カンマ区切りで複数選択）
     remarks TEXT  -- 備考
 );
