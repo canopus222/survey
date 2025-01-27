4d4bd4754e4ffdf585e609b7af89caac2e37867c
star_ <star_berry2s@yahoo.co.jp>
2025-01-27 18:35:36 +0900

    Update Teng schema

--- lib/survey/DB/Schema.pm
+++ lib/survey/DB/Schema.pm
@@ -7,10 +7,11 @@ use Teng::Schema::Declare;
 
 base_row_class 'survey::DB::Row';
 
+# surveyテーブルのスキーマを定義
 table {
-    name 'member';
-    pk 'id';
-    columns qw(id name);
+    name 'survey'; # テーブル名
+    pk 'id'; # テーブルのプライマリーキーを指定
+    columns qw(id name age job drinks remarks);
 };
 
 1;
